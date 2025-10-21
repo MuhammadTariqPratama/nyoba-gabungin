d3248ce058c6d9e34f95b2d952e83e8677f44abd
Tariq <muhammadthariqpratama@gmail.com>
2025-10-21 14:14:52 +0700

    perbaikan

--- seeders/20251019200455-alurBarang.js
+++ seeders/20251019200455-alurBarang.js
@@ -10,8 +10,8 @@ module.exports = {
         jumlah: 100,
         lokasiProduk: "Gudang Utama",
         keterangan: "Barang baru diterima dari pemasok.",
-        variasiID: 24,
-        adminID: 3,
+        variasiID: 5,
+        adminID: 2,
       },
       {
         jenisAlur: "Keluar",
@@ -19,8 +19,8 @@ module.exports = {
         jumlah: 30,
         lokasiProduk: "Toko Cabang 1",
         keterangan: "Pengiriman produk ke cabang.",
-        variasiID: 25,
-        adminID: 3,
+        variasiID: 6,
+        adminID: 2,
       },
       {
         jenisAlur: "Masuk",
@@ -28,8 +28,8 @@ module.exports = {
         jumlah: 50,
         lokasiProduk: "Gudang B",
         keterangan: "Restock produk dari supplier tambahan.",
-        variasiID: 26,
-        adminID: 3,
+        variasiID: 7,
+        adminID: 2,
       },
       {
         jenisAlur: "Keluar",
@@ -37,8 +37,8 @@ module.exports = {
         jumlah: 10,
         lokasiProduk: "Toko Online",
         keterangan: "Penjualan online (e-commerce).",
-        variasiID: 27,
-        adminID: 3,
+        variasiID: 8,
+        adminID: 2,
       },
     ]);
   },
